37012645cdc1e30f53c9eab6b281c0100c68ce71
= <=>
2022-06-25 18:03:24 +0200

    Add tailwind

---
+++ web/components/header-bar.tsx
@@ -0,0 +1,10 @@
+
+const HeaderBar = () => {
+    return (
+        <header className="bg-blue-800 p-3">
+            <h2 className="text-2xl text-white">Stammtisch</h2>
+        </header>
+    )
+}
+
+export default HeaderBar
