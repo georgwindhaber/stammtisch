66fb537a89c871329d91723500afe2f0ec61ccd1
= <=>
2022-06-25 17:25:48 +0200

    Ad test login page

---
+++ web/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>Stammtisch</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
