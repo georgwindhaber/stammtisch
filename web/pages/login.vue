f86baba730d4afd5ec8050e1a35c3e6b45617d23
georgwindhaber <georg.windhaber@gmail.com>
2022-08-16 09:33:46 +0200

    Add login error handling

--- web/pages/login.vue
+++ web/pages/login.vue
@@ -1,13 +1,25 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 import TextInput from "~/components/inputs/text-input.vue";
 import ActionButton from "~/components/action-button.vue";
+import Axios from "axios"
 
 const email = ref("");
 const password = ref("");
+const error = ref("")
 
-const login = () => {
-  console.log(email.value, password.value);
+const login = async () => {
+
+  try {
+
+    const response = await Axios.post('http://localhost:1337/api/auth/local', { identifier: email.value, password: password.value })
+    console.log(response)
+  } catch (err) {
+    if (err?.response?.data?.error?.name === 'ValidationError') {
+      error.value = "Falsche Email-Adresse oder Passwort"
+    }
+  }
+
 };
 </script>
 
@@ -24,5 +36,9 @@ const login = () => {
       <text-input type="password" name="password" v-model="password" />
     </div>
     <action-button @click="login">Login</action-button>
+
+    <div v-if="error" class="mt-5 p-3 bg-red-200 border-solid border border-red-400 rounded">
+      {{ error }}
+    </div>
   </div>
 </template>
