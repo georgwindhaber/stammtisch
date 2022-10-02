d932a740508320725ae73bd5dda5ac1dd756dd89
georgwindhaber <georg.windhaber@gmail.com>
2022-10-02 12:48:41 +0200

    Add filtered test-query

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,15 +1,31 @@
 import { TextField } from '@mui/material'
+import Axios from 'axios'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
 
- const router = useRouter()
+
+  const router = useRouter()
+  useEffect(() => {
+    if (!localStorage.jwt) {
+      router.push("/login")
+    } else {
+      const queryDrinks =async () => {
+        const drinks = await Axios.get("http://localhost:1337/api/drinks?filters[users_permissions_user][id]=1", {headers: {
+          Authorization: `Bearer ${localStorage.jwt}`
+        }})
+        console.log(drinks.data.data)
+      }
+      queryDrinks()
+    }
+  })
 
   return (
     <>
       <h1>Please log in</h1>
-      
+
     </>
   )
 }
---
+++ next/types/drink.ts
@@ -0,0 +1,9 @@
+type DrinkType = "Beer" | "Wine" | "Water" | "Softdrink";
+
+export type Drink = {
+    id: number,
+    type: DrinkType,
+    created: Date,
+    lastUpdate: Date,
+    user: "string"
+}
