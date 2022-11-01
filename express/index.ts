b2aa0497e5df33a42807231ff560a3255c777bb5
georgwindhaber <georg.windhaber@gmail.com>
2022-11-01 14:29:39 +0100

    Add postgresql

--- express/index.ts
+++ express/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { Client } from "pg";
 
-const app = express()
-const port = 3003
+const app = express();
+const port = 3003;
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
+  console.log("new req")
+  res.send("hello world");
+});
 
-app.listen(port, () => {
-    console.log("Listening on port " + port)
-})
+app.get("/login", (req, res) => {});
+
+app.get("/drink", (req, res) => {});
+
+app.listen(port, async () => {
+  const dbClient = new Client({ user: "postgres", password: "brogress" });
+  await dbClient.connect();
+
+  const res = await dbClient.query("select $1::text as message", [
+    "Hello world lletzzzz gooooo",
+  ]);
+  console.log(res.rows[0].message);
+
+  console.log("listening to routes");
+});
