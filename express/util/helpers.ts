3ea47831212fe0eb7415de9c1a2ccfdcfca0c63c
georgwindhaber <georg.windhaber@gmail.com>
2022-11-06 10:26:31 +0100

    Make all the files prettier

--- express/util/helpers.ts
+++ express/util/helpers.ts
@@ -1,11 +1,11 @@
-import camelcase from "lodash/camelcase"
+import camelcase from 'lodash/camelcase'
 
 export const mapToCamelCase = (object: any) => {
-    const returnObject: any = {}
+	const returnObject: any = {}
 
-    for(const index in object) {
-        returnObject[camelcase(index)] = object[index]
-    }
+	for (const index in object) {
+		returnObject[camelcase(index)] = object[index]
+	}
 
-    return returnObject
+	return returnObject
 }
