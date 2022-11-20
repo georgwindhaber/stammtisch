b91a0bcf9f95efdbba9b019e2b7be08d25aa0a92
georgwindhaber <georg.windhaber@gmail.com>
2022-11-20 15:44:40 +0100

    Upgrade to next 13

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app"
 import { AppBar } from "@mui/material"
+import localFont from "@next/font/local"
+
+// const Jest = localFont({ src: "./jost.ttf" })
+import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
