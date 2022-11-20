f809d07ac12d60abb9ff0029b505ed2cb6b22fc2
georgwindhaber <georg.windhaber@gmail.com>
2022-11-20 16:05:52 +0100

    Add mui theme

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app"
-import { AppBar } from "@mui/material"
+import { AppBar, ThemeProvider } from "@mui/material"
 
 import "../styles/global.scss"
+import { defaultTheme } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<ThemeProvider theme={defaultTheme}>
 			<AppBar />
 			<Component {...pageProps} />
-		</>
+		</ThemeProvider>
 	)
 }
 
