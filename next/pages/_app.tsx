520f850f0d4bd444fa1e7c5315b854a35cae9ee8
georgwindhaber <georg.windhaber@gmail.com>
2022-11-20 15:53:45 +0100

    Add global font

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from "next/app"
 import { AppBar } from "@mui/material"
-import localFont from "@next/font/local"
 
-// const Jest = localFont({ src: "./jost.ttf" })
 import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
