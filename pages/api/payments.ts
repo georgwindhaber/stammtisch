9bc7894be80da8cc6d6c4bbb1ad87e4777720f4a
Georg Windhaber <georg@journiapp.com>
2023-09-17 14:31:06 +0200

    Add payments

---
+++ pages/api/payments.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "./_base"
+
+export default async function payments(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === "GET" && typeof req.query.user === "string") {
+		const payments = await prisma.payments.findMany({
+			where: {
+				userId: parseInt(req.query.user),
+			},
+		})
+		res.send(payments)
+		return
+	} else if (req.method === "POST") {
+		const requests = []
+		for (const userPayment of req.body.userPayments) {
+			console.log(userPayment)
+			requests.push(
+				prisma.payments.create({
+					data: {
+						userId: userPayment.userId,
+						amount: userPayment.payment,
+					},
+				}),
+			)
+			requests.push(
+				prisma.user.update({
+					where: {
+						userId: userPayment.userId,
+					},
+					data: {
+						paymentSum: {
+							increment: userPayment.payment,
+						},
+					},
+				}),
+			)
+			await Promise.all(requests)
+		}
+		res.status(200).end()
+		return
+	} else {
+		res.status(405).end()
+	}
+}
--- pages/bezahlt.tsx
+++ pages/bezahlt.tsx
@@ -1,31 +1,94 @@
-import { Container, List, ListItem, Input } from "@mui/material"
+import { Container, List, ListItem, Input, Fab, styled } from "@mui/material"
 import type { NextPage } from "next"
 import { useBackend } from "../hooks/use-backend"
 import { User } from "@prisma/client"
 import { ListItemUser } from "../components/ListItemUser"
+import { Euro, SportsBar } from "@mui/icons-material"
+import { useState } from "react"
+
+const BottomDrawer = styled("section")({
+	position: "absolute",
+	bottom: 10,
+	display: "flex",
+	justifyContent: "center",
+	width: "100%",
+})
+
+const FabContainer = styled("div")({
+	position: "relative",
+	display: "flex",
+	alignItems: "center",
+})
 
 const Bezahlt: NextPage = () => {
 	const { data: users, fetch: fetchUsers } = useBackend<User[]>("/api/users")
+	const [userPayments, setUserPayments] = useState<Array<{ userId: number; payment: number }>>([])
+
+	const { fetch: payment } = useBackend<any>("/api/payments", {
+		method: "POST",
+		data: { userPayments },
+	})
+
+	const handlePaymentChange = (userId: number, payment: number) => {
+		if (!payment) {
+			payment = 0
+		}
+		const currentIndex = userPayments.findIndex((user) => user.userId === userId)
+		const newPayments = [...userPayments]
+
+		if (currentIndex === -1) {
+			newPayments.push({ userId, payment })
+		} else {
+			newPayments[currentIndex] = { userId, payment }
+		}
+
+		setUserPayments(newPayments.filter((user) => user.payment !== 0))
+	}
+
+	const handlePayment = async () => {
+		await payment()
+		await fetchUsers()
+		setUserPayments([])
+	}
 
 	return (
-		<Container fixed maxWidth="md">
-			<List dense>
-				{users &&
-					users
-						.sort((a, b) => b.drinkCount - a.drinkCount)
-						.map((user) => {
-							return (
-								<ListItem
-									key={user.userId}
-									disableGutters
-									secondaryAction={<Input type="number" size="small" placeholder="100" />}
-								>
-									<ListItemUser user={user} valueType="payments" />
-								</ListItem>
-							)
-						})}
-			</List>
-		</Container>
+		<>
+			<Container fixed maxWidth="md">
+				<List dense>
+					{users &&
+						users
+							.sort((a, b) => b.drinkCount - a.drinkCount)
+							.map((user) => {
+								return (
+									<ListItem
+										key={user.userId}
+										disableGutters
+										secondaryAction={
+											<Input
+												type="number"
+												size="small"
+												placeholder="100"
+												value={userPayments.find((u) => u.userId === user.userId)?.payment || ""}
+												onChange={(e) => {
+													handlePaymentChange(user.userId, parseInt(e.target.value))
+												}}
+											/>
+										}
+									>
+										<ListItemUser user={user} valueType="payments" />
+									</ListItem>
+								)
+							})}
+				</List>
+			</Container>
+			<BottomDrawer>
+				<FabContainer>
+					<Fab color="primary" onClick={handlePayment} disabled={userPayments.length === 0}>
+						<Euro fontSize="large" />
+					</Fab>
+				</FabContainer>
+			</BottomDrawer>
+		</>
 	)
 }
 
