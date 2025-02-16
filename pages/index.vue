c9b1b5036aa0e96e32d1e5ac797dae536fda59c0
Georg Windhaber <georg.windhaber@gmail.com>
2025-02-16 09:25:15 +0100

    Add members table

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const selected = ref([]);
+
+const colums = [
+  {
+    key: "name",
+    label: "Name",
+  },
+  { key: "drinks", label: "Bier" },
+];
+
+const members = [
+  { name: "Hannes Apfelbacher", drinks: 641 },
+  { name: "Roman Froihofer", drinks: 495 },
+  { name: "Christian Rinnhofer", drinks: 472 },
+  { name: "Philipp Paier", drinks: 401 },
+  { name: "Hannes Buchebner", drinks: 272 },
+  { name: "Matthias Meierhofer", drinks: 214 },
+  { name: "Markus Buchebner", drinks: 164 },
+];
+</script>
 
 <template>
-  <h1>Kellerbierrunde</h1>
+  <UContainer>
+    <UTable v-model="selected" :rows="members" :columns="colums" />
+  </UContainer>
 </template>
