955c908289c4ac303854baa13e85844eb460d95e
Georg Windhaber <georg.windhaber@gmail.com>
2025-03-01 14:58:00 +0100

    Change tab size

--- components/material-design/md-tab.vue
+++ components/material-design/md-tab.vue
@@ -4,7 +4,7 @@ defineProps<{ selected?: boolean }>();
 
 <template>
   <button
-    class="bg-surface relative min-w-32 h-12 flex justify-center items-center gap-2"
+    class="bg-surface relative min-w-28 h-12 flex justify-center items-center gap-2"
     :class="[selected ? 'text-on-surface' : 'text-on-surface-variant']"
   >
     <slot></slot>
