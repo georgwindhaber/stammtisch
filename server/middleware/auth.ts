b236a644357cf802798f1aae25f3b67a0a53d658
georg <georg.windhaber@gmail.com>
2026-02-16 12:12:04 +0100

    Skip auth in dev

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,6 +3,10 @@ import { getServerSession } from "#auth";
 export default eventHandler(async (event) => {
   const apiRegex = /\/api(?!\/auth)\b/;
 
+  if (process.env.NODE_ENV === "development") {
+    return;
+  }
+
   if (apiRegex.test(event.path) && !event.path.includes("drinks")) {
     const session = await getServerSession(event);
     if (!session) {
